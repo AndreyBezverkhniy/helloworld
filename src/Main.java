316f91dd702507791071b1f6bcf33916018ca502
banbezv <banbezv@mail.ru>
2025-01-16 22:18:10 +0300

    HW-2 code auto formatting

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,68 @@
 public class Main {
     public static void main(String[] args) {
         // Task 1
-        var dog=8.0;
-        var cat=3.6;
-        var paper=763789;
-        System.out.println("dog="+dog);
-        System.out.println("cat="+cat);
-        System.out.println("paper="+paper);
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog=" + dog);
+        System.out.println("cat=" + cat);
+        System.out.println("paper=" + paper);
         System.out.println();
         // Task 2
-        dog=dog+4;
-        cat=cat+4;
-        paper=paper+4;
-        System.out.println("dog="+dog);
-        System.out.println("cat="+cat);
-        System.out.println("paper="+paper);
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("dog=" + dog);
+        System.out.println("cat=" + cat);
+        System.out.println("paper=" + paper);
         System.out.println();
         // Task 3
-        dog=dog-3.5;
-        cat=cat-1.6;
-        paper=paper-7639;
-        System.out.println("dog="+dog);
-        System.out.println("cat="+cat);
-        System.out.println("paper="+paper);
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("dog=" + dog);
+        System.out.println("cat=" + cat);
+        System.out.println("paper=" + paper);
         System.out.println();
         // Task 4
-        var friend=19;
-        System.out.println("friend="+friend);
-        friend=friend+2;
-        System.out.println("friend="+friend);
-        friend=friend/7;
-        System.out.println("friend="+friend);
+        var friend = 19;
+        System.out.println("friend=" + friend);
+        friend = friend + 2;
+        System.out.println("friend=" + friend);
+        friend = friend / 7;
+        System.out.println("friend=" + friend);
         System.out.println();
         // Task 5
-        var frog=3.5;
-        System.out.println("frog="+frog);
-        frog=frog*10;
-        System.out.println("frog="+frog);
-        frog=frog/3.5;
-        System.out.println("frog="+frog);
-        frog=frog+4;
-        System.out.println("frog="+frog);
+        var frog = 3.5;
+        System.out.println("frog=" + frog);
+        frog = frog * 10;
+        System.out.println("frog=" + frog);
+        frog = frog / 3.5;
+        System.out.println("frog=" + frog);
+        frog = frog + 4;
+        System.out.println("frog=" + frog);
         System.out.println();
         // Task 6
-        var weight1=78.2;
-        var weight2=82.7;
-        System.out.println("Weight of first = "+weight1);
-        System.out.println("Weight of second = "+weight2);
-        var sum=weight1+weight2;
-        System.out.println("Common weight = "+sum);
-        var dif=weight2-weight1;
-        System.out.println("Difference = "+dif);
+        var weight1 = 78.2;
+        var weight2 = 82.7;
+        System.out.println("Weight of first = " + weight1);
+        System.out.println("Weight of second = " + weight2);
+        var sum = weight1 + weight2;
+        System.out.println("Common weight = " + sum);
+        var dif = weight2 - weight1;
+        System.out.println("Difference = " + dif);
         System.out.println();
         // Task 7
-        var mod=weight2%weight1;
-        System.out.println("mod="+mod);
+        var mod = weight2 % weight1;
+        System.out.println("mod=" + mod);
         System.out.println();
         // Task 8
-        var workers=640/8;
-        System.out.println("workers="+workers);
-        var workers2=workers+94;
-        var hours=640-640%workers2;
-        System.out.println("If at compain work "+workers2+" workers "+
-                "then common hours divisible between them is "+hours+" hours");
+        var workers = 640 / 8;
+        System.out.println("workers=" + workers);
+        var workers2 = workers + 94;
+        var hours = 640 - 640 % workers2;
+        System.out.println("If at compain work " + workers2 + " workers " +
+                "then common hours divisible between them is " + hours + " hours");
         System.out.println();
 
     }
